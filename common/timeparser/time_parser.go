9614a71e05aecf5041c6774fc13df1e3d7cba974
qyzzy <42561599+qyzzy@users.noreply.github.com>
2022-08-07 09:05:49 +0800

    update time parser

--- common/timeparser/time_parser.go
+++ common/timeparser/time_parser.go
@@ -4,7 +4,12 @@ import (
 	"time"
 )
 
-func UnixToTime(data int64) (dataTime time.Time) {
-	dataTime = time.Unix(data/1000, 0)
-	return
+func UnixToTime(data int64) time.Time {
+	dataTime := time.Unix(data/1000, 0)
+	return dataTime
+}
+
+func TimeToUnix(e time.Time) int64 {
+	timeUnix, _ := time.Parse("2006-01-02 15:04:05", e.Format("2006-01-02 15:04:05"))
+	return timeUnix.UnixNano() / 1e6
 }
